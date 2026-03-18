4d4c2bc134085aa7abe20a684a6a3c31cce34292
NatnaelAyele <n.eticha@alustudent.com>
2026-03-18 10:45:55 +0300

    added middleware to allow communication between frontend and backend applications

--- app/main.py
+++ app/main.py
@@ -4,10 +4,21 @@ from app.api.auth_routes import router as auth_router
 from app.database.db import Base, engine
 from app.models import models
 from app.api.test import app as test_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 app.include_router(auth_router)
 app.include_router(test_router)
